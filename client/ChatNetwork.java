9b9106fedacdfd9b64e6835a7aef84a45fde66b0
Macha <Macha.hack@gmail.com>
2009-07-25 12:39:16 +0100

    Added GPL notice to all files.

--- client/ChatNetwork.java
+++ client/ChatNetwork.java
@@ -1,5 +1,22 @@
 /*
- * ChatNetwork.java
+ *  Machat
+ *
+ *  Copyright (C) Macha <macha.hack@gmail.com>
+ *
+ *  This file is part of Machat.
+ *
+ *  Machat is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Machat is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Machat.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package machat.client;
--- client/ContactsWindow.java
+++ client/ContactsWindow.java
@@ -1,7 +1,22 @@
 /*
- * ContactsWindow.java
+ *  Machat
  *
- * Created on 13-Jul-2009, 11:33:46
+ *  Copyright (C) Macha <macha.hack@gmail.com>
+ *
+ *  This file is part of Machat.
+ *
+ *  Machat is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Machat is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Machat.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package machat.client;
